e139073ddffc2882f1d3276d9d5cf90de7541761
sanlinhtik3 <sanlinhtike@Sans-MacBook-Pro.local>
2023-05-12 14:51:58 +0630

    REACT-BASIC-React-JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 function App() {
-  return <h1>Hello Hi</h1>
+
+  const fruit = "Apple"
+
+  return (
+    <div>
+      <h1>Hello Hi</h1>
+      <h2>Hello</h2>
+      <img src="" alt="" />
+      <input type="text" />
+
+      <h4>{1 + 1}</h4>
+
+      <h1>List</h1>
+      <ul>
+        <li>{fruit}</li>
+        <li>Orange</li>
+        <li>Mango</li>
+      </ul>
+    </div>
+  );
+  
 }
 
 export default App
