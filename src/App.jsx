09df2bd674211dcad60436bd2ff449ce9e87dee4
sanlinhtik3 <sanlinhtike@Sans-MacBook-Pro.local>
2023-05-13 11:16:20 +0630

    REACT-BASIC-React-Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
 import React from "react";
 
-// Class Component
-// class Go extends React.Component {
-//   render() {
-//     return <h1>Class Component</h1>
-//   }
-// }
+const Phone = ({ children }) => {
+
+  // const info = ['ios 14', 'black']
+  // const [name, color] = info
+  // console.log(color)
+
+  // const {brand} = props
+  // console.log(brand)
+
+  // console.log(brand)
 
 
-// Function Component
-// function App() {
-//   return (
-//     <div>
-//       <h1>Ract Components</h1>
-//     </div>
-//   );
-// }
-
-const Phone = (props) => {
-  return <h1>Hello Phone {props.brand} </h1>;
+  return (
+    <h1>{children}</h1>
+  );
 }
 
 const App = () => {
   return (
     <div>
-      <h1>Ract Components</h1>
-      <Phone brand="iPhone" />
+      <h1>Ract Props</h1>
+      <Phone>Phone</Phone>
     </div>
   );
 };
