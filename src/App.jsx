d8fe92411a899958b3185705b560980bbe1eefef
sanlinhtik3 <sanlinhtike@Sans-MacBook-Pro.local>
2023-06-06 02:31:30 +0630

    REACT-BASIC-React-Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react"
+import TextInput from './components/TextInput'
 
 const App = () => {
-  const shopping = ['BMW', 'Ford', 'Audi', 'Orange', 'Mango']
-  console.log(shopping)
+  const [inputs, setInputs] = useState({})
+
+  // Destructuring
+  // const fruits = ['apple', 'orange', 'mango']
+  // const [a, b, c] = fruits
+  // console.log(c)
+
+
+  const handleChange = (event) => {
+    // const name = event.target.name
+    // const value = event.target.value;
+
+    const {name, value} = event.target
+    setInputs((pre) => ({...pre, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(inputs)
+  }
+
 
     return (
       <>
-        <h1>React Lists</h1>
-        <ul>
-          {shopping.map((sh,index) => (
-            <li key={index}>{sh}</li>
-          ))}
-        </ul>
+        <h1>React Form</h1>
+        <form onSubmit={handleSubmit}>
+          <TextInput name="username" onChange={handleChange} />
+          <TextInput name="email" onChange={handleChange} />
+          <button>Signup</button>
+        </form>
       </>
     );
 }
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,10 @@
+
+const TextInput = ({...props}) => {
+  return (
+    <>
+        <input {...props} type="text" />
+    </>
+  )
+}
+
+export default TextInput
